2e1927e14e9fe3472148cb5994ff4d017bd9da9c
IDGS-803-21000448 <carene2803@gmail.com>
2024-01-26 18:49:34 -0600

    CEMR 28/01/2024 MACROS

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -43,6 +43,40 @@ def alumnos():
 def maestros():
     return render_template("maestros.html")
 
+
+@app.route("/multiplicar", methods=["GET", "POST"])
+def multiplicar():
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+
+        return f"<h1> La multiplicacion es: {str(int(num1) * int(num2))} </h1>"
+    else:
+        return '''
+                <form action="/multiplicar" method="POST">
+                    <label>N1: </label>
+                    <input type= "text" name="n1"/> </br>
+                    <label>N2: </label>
+                    <input type= "text" name="n2"/> </br>
+                    <input type="submit"/>
+                </form>
+             '''
+
+@app.route("/formulario1")
+def formulario():
+    return render_template("formulario1.html")
+
+@app.route("/resultado",  methods=["GET", "POST"])
+def resultado():
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+
+        return f"<h1> La multiplicacion es: {str(int(num1) * int(num2))} </h1>"
+
+@app.route("/formulario2")
+def formulario2():
+    return render_template("formulario2.html")
 
 
 if __name__ == "__main__":
